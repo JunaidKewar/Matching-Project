1fefd2286978f3ba49c141b3ca14ab6293340318
Junaid Kewar <junaidkewar@gmail.com>
2023-01-14 22:24:25 +0530

    Generated distance matrix for all values of (M,m)

--- testfile.py
+++ testfile.py
@@ -3,6 +3,16 @@ from pprint import pprint
 
 listDict = [    { 'Software development':0, 'Core':1, 'Finance':2, 'Consultancy':3, 'ML/AI':4 },    { 'Research groups':0, 'Technical societies':1, 'Cells':2, 'Fests':3, 'Cultural societies':4, 'Miscellaneous societies/clubs':5 },    { 'Research':0, 'Corporate':1, 'Start-up':2, 'MBA':3, 'UPSC':4 },{ 'Hindi':0, 'Malayalam':1, 'Bengali':2, 'Tamil':3, 'Telugu':4 }]
 
+def calcDvalue(mentor, student):
+    commonValue = 0
+    for x in range(0,len(mentor)):
+        for y in range(0,len(mentor[x]):
+            if (mentor[x][y] == mentee[x][y]):
+                commonValue += 1
+    print commonValue
+    return commonValue
+
+
 def rowInfo(row, CData):
     data = []
     for i, options in enumerate(row):
@@ -35,6 +45,12 @@ def preprocess(csvfile):
         return CData
 
 if __name__ == "__main__":
-    csvfile = 'Questionnaire.csv'
+    csvfile = 'Response_Mentees.csv'
     studentCData = preprocess(csvfile)
     pprint(studentCData)
+
+    csvfile = 'Response_Mentors.csv'
+    mentorsCData = preprocess(csvfile)
+    pprint(mentorsCData)
+
+    calcDvalue(studentCData[3], mentorsCData[0])
